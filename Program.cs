f40880f3ad66d950dbdc07e908a3e43add249d87
Zeeshan <gecezeeshan@gmail.com>
2022-02-07 19:11:40 +0400

    key vault

--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@ namespace SalesManagement
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
+//.ConfigureAppConfiguration((context, config) =>
+//{
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
