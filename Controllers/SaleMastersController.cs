46064df4f90a5ed5a6585ac1deff0832226ea971
Zeeshan <gecezeeshan@gmail.com>
2022-02-09 16:22:23 +0400

    asdf

--- Controllers/SaleMastersController.cs
+++ Controllers/SaleMastersController.cs
@@ -34,7 +34,7 @@ namespace SalesManagement.Controllers
             {
                 DateTime date = CurrentDate.Value;
                 sm = await _context.GetSalesAsync(date);
-                ViewData["CurrentDate"] = date.ToString("dd MMM yyyy");
+                ViewData["CurrentDate"] = date.ToString("dd/MM/yyyy");
             }
            
             
